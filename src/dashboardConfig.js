d891def721ce3feb3bb659c70f9c49d3d7b68685
Jared Brown <jared.brown@elementthree.com>
2020-10-29 22:38:35 -0400

    Implement dashboard deploys

---
+++ src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Website",
+            apiId: "668bdafb-52b2-48e1-8849-8db1c8d204dd",
+            buildHookId: "5f9b7b0d4b8985431819911a",
+            name: "alexandrabrown",
+          },
+        ],
+      },
+    },
+  ],
+};
